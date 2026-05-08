de940e1153cf8fcd39d58d5386095464bccce875
Tiago Victor <tiagodev.profissional@gmail.com>
2026-05-08 19:23:27 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import { PrismaClient  } from '@prisma/client';
+import cors from 'cors';
+
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+        data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age 
+        }
+   });
+   res.status(201).send('Usuário adicionado ao MongoDB com sucesso!');
+    
+});
+
+app.get('/usuarios', async (req, res) => {
+    const allUsers = await prisma.user.findMany(); //Busca do banco 
+    res.json(allUsers);
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).send('Usuário deletado com sucesso!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+/*eeeeeeeeeeeeeeeeee
+tiago
+
+5JzFClxoSGM1prgx
+
+
+*/
